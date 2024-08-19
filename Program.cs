8b2a9205050a750f4d9659ba350a315eded4d677
Fertilesoil <gasnbloop@gmail.com>
2024-08-19 14:28:32 -0300

    Testando autenticação só por cookies

--- Program.cs
+++ Program.cs
@@ -309,7 +309,7 @@ else
 
 app.UseCookiePolicy(new CookiePolicyOptions
 {
-    MinimumSameSitePolicy = SameSiteMode.Lax,
+    MinimumSameSitePolicy = SameSiteMode.None,
     HttpOnly = HttpOnlyPolicy.Always,
     Secure = CookieSecurePolicy.Always
 });
